01c38618e25bf85d2a4080a243c79a5dc2c5fc79
gav974ordi1 <eclapier@msn.com>
2022-01-12 10:25:13 +0400

    ajout d'une fonction pour changer les photos

--- lib/main.dart
+++ lib/main.dart
@@ -12,14 +12,19 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+
+
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
           home:Scaffold(
             appBar: AppBar(
-              title:const Text('Ask me anything',
-                textAlign: TextAlign.center,),
+              title:const Center(
+                child: Text('Ask me anything',
+                  textAlign: TextAlign.center,),
+              ),
               backgroundColor: Colors.blueAccent,
             ),
             body:sectionImage,
@@ -28,18 +33,24 @@ class _MyAppState extends State<MyApp> {
     );
   }
 }
+int numberMagic= 1;
+Widget sectionImage = Column(
+  mainAxisAlignment: MainAxisAlignment.center,
+  children: [
+    Container(
+      child: TextButton(
+          onPressed: () {
+            _increment(); print('numberMagic');  },
+          child: Image.asset("assets/images/ball$numberMagic.png")
+      ),
+    )
 
-Widget sectionImage = Container(
-  child: Column(
-    mainAxisAlignment: MainAxisAlignment.center,
-    children: [
-      Container(
-        child: TextButton(
-            onPressed: () {  },
-            child: Image.asset("assets/images/ball1.png")
-        ),
-      )
-
-    ],
-  ),
+  ],
 );
+int _increment() {
+    if (numberMagic < 6) {
+   return numberMagic++;
+  } else {
+   return numberMagic = 1;
+  }
+  }
